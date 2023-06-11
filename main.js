b3dae356ec82cd7aab08bc345c2a0bd39bf82b23
t3ddesigncompany <115772710+t3ddesigncompany@users.noreply.github.com>
2023-06-10 21:09:12 -0400

    commit

---
+++ main.js
@@ -0,0 +1,98 @@
+window.sr = ScrollReveal();
+
+sr.reveal('.icono', {
+    duration: 4000,
+    origin: 'bottom',
+    distance: '-200px',
+});
+
+sr.reveal('.titulo', {
+    duration: 4000,
+    origin: 'lef',
+    distance: '-200px',
+});
+
+sr.reveal('.subtitulo', {
+    duration: 4000,
+    origin: 'right',
+    distance: '200px',
+});
+
+sr.reveal('.boton', {
+    duration: 3000,
+    origin: 'top',
+    distance: '-200px',
+});
+
+sr.reveal('.sobre-nosotros', {
+    duration: 4000,
+    origin: 'lef',
+    distance: '-300px',
+});
+
+sr.reveal('.slogan', {
+    duration: 3000,
+    origin: 'right',
+    distance: '200px',
+});
+
+sr.reveal('.parrafo', {
+    duration: 3000,
+});
+
+sr.reveal('.cards', {
+    duration: 4000,
+});
+
+sr.reveal('.cards img', {
+    duration: 4000,
+    rotate: {
+        x: 70,
+        z: 140
+    }
+
+});
+
+sr.reveal('.cards h4', {
+    duration: 3000,
+    origin: 'lef',
+    distance: '-300px',
+});
+
+sr.reveal('.cards p', {
+    duration: 3000,
+    origin: 'lef',
+    distance: '-300px',
+});
+
+sr.reveal('.titulo-patrocinadores', {
+    duration: 4000,
+    origin: 'lef',
+    distance: '-200px',
+});
+
+sr.reveal('.subtitulo-patrocinadores', {
+    duration: 4000,
+    origin: 'right',
+    distance: '200px',
+});
+
+sr.reveal('.cards2', {
+    duration: 4000,
+});
+
+sr.reveal('.titulo-footer', {
+    duration: 4000,
+    origin: 'lef',
+    distance: '-200px',
+});
+
+sr.reveal('.subtitulo-footer', {
+    duration: 4000,
+    origin: 'right',
+    distance: '200px',
+});
+
+sr.reveal('.footer', {
+    duration: 4000,
+});
